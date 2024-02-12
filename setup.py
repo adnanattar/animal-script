9cd127e3502d5ec4fe286956f1b4ec7d01c6ce6e
Adnan Attar <attaradnan132@gmail.com>
2024-02-13 02:05:59 +0530

    Setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='animal-script',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
+    # other arguments omitted
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         # Add any dependencies here
     ],
