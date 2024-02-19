69ca80d4b26790714350a149b4147b813f8938db
Adnan Attar <attaradnan132@gmail.com>
2024-02-19 13:01:25 +0530

    Version Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='animal-script',
-    version='0.7',
+    version='0.8',
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
